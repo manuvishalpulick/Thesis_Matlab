0411c60e2938d61af60aadd21ff60c79d0fceb5e
manuvishalpulick <45491601+manuvishalpulick@users.noreply.github.com>
2019-02-24 15:06:10 +0100

    save

--- move_results.m
+++ move_results.m
@@ -12,16 +12,16 @@ function move_results(mk,Tmp)
            movefile('Rupture_instant*',mk)                 % move all the image files to that directory 
 %           movefile(strcat('*_',num2str(L_flat),'_*.mat'),mk)                 % move the .mat file to that directory
 %           movefile('wave_num_fig*',mk)                 % move all the wave number energy evolution files to that directory
-          movefile('Energy_evolutionfig*',mk))                 %movee energy plots to mk
+          movefile('Energy_evolutionfig*',mk)                 %movee energy plots to mk
 %          movefile('omegafig*',mk)                 %movee omega plots to mk
           
     else
-          %movefile('Evolution_subplot*',mk)                 % move all the video files to that directory
+          movefile('Evolution_subplot*',mk)                 % move all the video files to that directory
           %movefile('filmEvolution*',mk)                 % move all the video files to that directory
           %movefile('Fourier_evolution*',mk)                 % move all the video files to that directory
-          %movefile('harmonic_evolution*',mk)                 % move all the image files to that directory
-          %movefile('Final_Sk_snapshot*',mk)                 % move all the image files to that directory
-          %movefile('Maximum_energy_frequency*',mk)                 % move all the image files to that directory
+          movefile('harmonic_evolution*',mk)                 % move all the image files to that directory
+          movefile('Final_Sk_snapshot*',mk)                 % move all the image files to that directory
+          movefile('Maximum_energy_frequency*',mk)                 % move all the image files to that directory
           %movefile(strcat('*_',num2str(L_flat),'_*.mat'),mk)                 % move the .mat file to that directory
           %movefile('wave_num_fig*',mk)                 % move all the wave number energy evolution files to that directory
           movefile('Energy_evolutionfig*',mk)                 %movee energy plots to mk
